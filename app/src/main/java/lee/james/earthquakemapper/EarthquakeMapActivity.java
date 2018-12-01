8d583676c4c85bcef67f72f53f590186ac2142b2
James Lee <jamesl33info@gmail.com>
2018-12-01 16:26:39 +0000

    The focused earthquake is now drawn in a different color

--- app/src/main/java/lee/james/earthquakemapper/EarthquakeMapActivity.java
+++ app/src/main/java/lee/james/earthquakemapper/EarthquakeMapActivity.java
@@ -6,18 +6,16 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class EarthquakeMapActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -27,6 +25,7 @@ public class EarthquakeMapActivity extends FragmentActivity implements OnMapRead
     private GoogleMap googleMap;
     private Integer currentEarthquake = 0;
     private ArrayList<Earthquake> earthquakes;
+    private ArrayList<Circle> earthquakeMarkers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,12 +56,12 @@ public class EarthquakeMapActivity extends FragmentActivity implements OnMapRead
             // TODO - Generate a heatmap
         } else {
             for (Earthquake earthquake : this.earthquakes) {
-                this.googleMap.addCircle(new CircleOptions()
+                this.earthquakeMarkers.add(this.googleMap.addCircle(new CircleOptions()
                         .center(new LatLng(earthquake.getLatitude(), earthquake.getLongitude()))
                         .radius(100000 * earthquake.getMagnitude())
                         // TODO - Add a user preference to allow the user to pick a color
                         .strokeColor(Color.argb(50, 255, 0, 0))
-                        .fillColor(Color.argb(100, 255, 0, 0)));
+                        .fillColor(Color.argb(100, 255, 0, 0))));
 
                 this.focusCurrentEarthquake(null);
             }
@@ -70,6 +69,20 @@ public class EarthquakeMapActivity extends FragmentActivity implements OnMapRead
     }
 
     public void focusCurrentEarthquake(View view) {
+        if (currentEarthquake == 0) {
+            this.earthquakeMarkers.get(this.earthquakeMarkers.size() - 1).setStrokeColor(Color.argb(50, 255, 0, 0));
+            this.earthquakeMarkers.get(this.earthquakeMarkers.size() - 1).setFillColor(Color.argb(100, 255, 0, 0));
+            this.earthquakeMarkers.get(this.earthquakeMarkers.size() - 1).setZIndex(0);
+        } else {
+            this.earthquakeMarkers.get(currentEarthquake - 1).setStrokeColor(Color.argb(50, 255, 0, 0));
+            this.earthquakeMarkers.get(currentEarthquake - 1).setFillColor(Color.argb(100, 255, 0, 0));
+            this.earthquakeMarkers.get(currentEarthquake - 1).setZIndex(0);
+        }
+
+        this.earthquakeMarkers.get(currentEarthquake).setStrokeColor(Color.argb(50, 0, 0, 255));
+        this.earthquakeMarkers.get(currentEarthquake).setFillColor(Color.argb(100, 0, 0, 255));
+        this.earthquakeMarkers.get(currentEarthquake).setZIndex(Float.POSITIVE_INFINITY);
+
         this.googleMap.animateCamera(
                 CameraUpdateFactory.newLatLng(new LatLng(this.earthquakes.get(this.currentEarthquake).getLatitude(), this.earthquakes.get(this.currentEarthquake).getLongitude())),
                 400,
